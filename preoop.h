c6f031fd0aace7bea0b39f0137c69d8f6fb98d82
CTurt <cturt@users.noreply.github.com>
2015-05-25 18:35:07 +0100

    Support VA_ARGs on init methods

--- preoop.h
+++ preoop.h
@@ -7,8 +7,8 @@
 #define objectList(a, ...) _Generic((a), ##__VA_ARGS__)
 #define objectEntry(a, b) a *: b##_##a
 
-#define initCall(a) init_##a(malloc(sizeof(a)))
-#define new(a) initCall(a)
+#define initCall(a, ...) init_##a(malloc(sizeof(a)), ##__VA_ARGS__)
+#define new(a, ...) initCall(a, ##__VA_ARGS__)
 
 #define method(a, b, ...) b##_##a(a *self, ##__VA_ARGS__)
 #define invoke(a, b, ...) objects(a, b)(a, ##__VA_ARGS__)
